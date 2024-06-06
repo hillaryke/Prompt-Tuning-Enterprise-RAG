1594dd91b59dd34133f5b7b0102366a30a729667
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-06 13:33:52 +0300

    invoke rag chain

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from app.rag.docs_loader import load_and_split_markdown, load_and_split_md
 from app.rag.rag_worker import rag_chain
 
 def main():
-    # docs = load_and_split_markdown('data/docs/bank_name_docs.md', [('##', '###')])
-    docs = load_and_split_md('data/books')
-    print(docs[:5])
-    return docs
+    answer = rag_chain.invoke("Why is the Human race odd")
+    print(answer)
 
 if __name__ == '__main__':
     main()
