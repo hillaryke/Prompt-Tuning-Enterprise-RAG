da3f6a6658097c38d6a25c4363d18ab8d7bf7156
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-06 10:28:56 +0300

    load other md files

--- app/rag/docs_loader.py
+++ app/rag/docs_loader.py
@@ -14,3 +14,28 @@ def load_and_split_markdown(filepath: str, splitter: List[Tuple[str, str]]):
     md_header_splits = markdown_splitter.split_text(docs[0].page_content)
     return md_header_splits
     
+def load_and_split_md(DATA_PATH: str):
+    documents = load_documents(DATA_PATH)
+    chunks = split_text(documents)
+    return chunks
+
+def load_documents(DATA_PATH: str):
+    loader = DirectoryLoader(DATA_PATH, glob="*.md")
+    documents = loader.load()
+    return documents
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=400,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    document = chunks[10]
+    print(document.page_content)
+    print(document.metadata)
+
+    return chunks
