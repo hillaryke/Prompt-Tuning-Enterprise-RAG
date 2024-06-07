1d39a34e5687c7bc44a2a9939b42901473c3fcc7
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-07 16:28:21 +0300

    use langchain to generate test cases

--- app/test_cases/generate_test_cases.py
+++ app/test_cases/generate_test_cases.py
@@ -1,3 +1,8 @@
+from langchain_core.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
 from app.utils.chat_models import ModelFactory
 from langchain.schema import HumanMessage
 from app.test_cases.models import TestCase
@@ -6,12 +11,9 @@ TEMPERATURE = 1.5
 model_factory = ModelFactory(TEMPERATURE)
 
 create_test_case_prompt = """
-        Your job is to create a test case for a given task. The task is a description of a use-case.
+        Your job is to create a test case for a given task and it's expected output. The task is a description of a use-case.
 
-        The test case you will be creating will be for freeform tasks, such as generating a landing page headline, an intro paragraph, solving a math problem, etc.
-
-        The test case is a specific example of the task. It should be a specific example of the task, but not too specific. 
-        It should be general enough that it can be used to test the AI's ability to perform the task.
+        It should be general enough that it can be used to test the AI's ability to perform the task in retreiving documents.
         It should never actually complete the task, but it should be a good example of the task.
 
         Example:
@@ -30,13 +32,15 @@ create_test_case_prompt = """
         Test case: "Nike shoes"
         Test case: "A case for iPhones that's velvety smooth and very durable"
 
-        You will be graded based on the performance of your test case... but don't cheat! You cannot include specifics about the task in your test case. Any test cases with examples will be disqualified.
+        You will be graded based on the performance of your test case and expected output... but don't cheat! You cannot include specifics about the task in your test case. Any test cases with examples will be disqualified.
         Be really creative! The most creative test cases will be rewarded.
 
-        YOU NEVER OUTPUT SOMETHING THAT COMPLETES THE TASK. ONLY A TEST CASE.
+        YOU NEVER OUTPUT SOMETHING THAT COMPLETES THE TASK. ONLY A TEST CASE AND ITS EXPECTED OUTPUT.
 
-        Most importantly, output NOTHING but the test case. Do not include anything else in your message.
-        You only output one test case per message, without quotes.
+        Most importantly, output NOTHING but the test case and expected output. Do not include anything else in your message.
+        Each test case should include:
+        * Scenario: A clear description of the situation or input to be tested.
+        * Expected output: The ideal or expected output from the system.
 """
 
 def generate_test_cases(task_description, amount=3):
@@ -56,16 +60,38 @@ def generate_test_cases(task_description, amount=3):
     Expected output: [Expected output]
     """
 
-    model = model_factory.get_chat_openai()
+    llm = model_factory.get_chat_openai()
 
-    messages = [
-        HumanMessage(
-            content=prompt_template.format(amount=amount, description=task_description)
-        )
-    ]
+    system_message_prompt = SystemMessagePromptTemplate.from_template(create_test_case_prompt)
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
+        """
+            Please generate {amount} diverse test cases for the following task:
+            Task (task description): {task_description} 
+            Then the program should output:
 
-    response = model.invoke(messages)
-    generated_text = response.content
+            Remember, always output only the test cases and exact expected output for each test case.
+
+            Format each test case like this:
+            Scenario: [Scenario description]
+            Expected output: [Expected output]
+        """
+    )
+
+    # messages = [
+    #     HumanMessage(
+    #         content=prompt_template.format(amount=amount, description=task_description)
+    #     )
+    # ]
+
+    # response = model.invoke(messages)
+    # generated_text = response.content
+    chat_prompt_template = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+    chain = chat_prompt_template | llm | StrOutputParser()
+
+    generated_text = chain.invoke({"task_description": task_description, "amount": amount})
+
+    print(generated_text)
 
     test_cases = []
     for case_str in generated_text.split("Scenario:"):
