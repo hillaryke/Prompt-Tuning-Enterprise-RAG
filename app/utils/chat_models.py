90d62fea779a359899debcecb0362302d76d8988
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-07 18:02:17 +0300

    Load api keys in chat models

--- app/utils/chat_models.py
+++ app/utils/chat_models.py
@@ -1,8 +1,12 @@
+from json import load
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+from src.env_loader import load_api_key
+
 class ModelFactory:
   def __init__(self, temperature=1.5, openai_model_name="gpt-3.5-turbo", gemini_model_name="gemini-pro"):
+    load_api_key()
     self.temperature = temperature
     self.openai_model_name = openai_model_name
     self.gemini_model_name = gemini_model_name
