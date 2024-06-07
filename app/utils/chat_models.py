5075e7a9f61519486092ff9dbd2d42697774645a
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-07 14:43:13 +0300

    pass in model_name as optional argument

--- app/utils/chat_models.py
+++ app/utils/chat_models.py
@@ -2,19 +2,18 @@ from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 class ModelFactory:
-    def __init__(self, temperature=1.5):
-      self.temperature = temperature
+  def __init__(self, temperature=1.5, openai_model_name="gpt-3.5-turbo", gemini_model_name="gemini-pro"):
+    self.temperature = temperature
+    self.openai_model_name = openai_model_name
+    self.gemini_model_name = gemini_model_name
 
-    def get_chat_openai(self, model_name="gpt-3.5-turbo"):
-        return ChatOpenAI(model_name=model_name, temperature=self.temperature)
+  def get_chat_openai(self):
+    return ChatOpenAI(model_name=self.openai_model_name, temperature=self.temperature)
 
-    # If there is no environment variable set for the API key, you can pass the API
-    # key to the parameter `google_api_key` of the `ChatGoogleGenerativeAI` function:
-    # `google_api_key="key"`.
-    def get_chat_gemini(self, model="gemini-pro", top_p=0.85, google_api_key=None):
-        return ChatGoogleGenerativeAI(
-                    model=model, 
-                    temperature=self.temperature, 
-                    top_p=top_p, 
-                    google_api_key=google_api_key
-        )
+  def get_chat_gemini(self, top_p=0.85, google_api_key=None):
+    return ChatGoogleGenerativeAI(
+      model=self.gemini_model_name, 
+      temperature=self.temperature, 
+      top_p=top_p, 
+      google_api_key=google_api_key
+    )
