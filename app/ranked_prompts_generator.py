7208bc3946bc636aeb1048512c9d194525647e23
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-09 16:43:56 +0300

    add examples folder

--- app/ranked_prompts_generator.py
+++ app/ranked_prompts_generator.py
@@ -1,11 +1,10 @@
-from app.test_cases import TestCase, generate_test_cases
+from app.test_cases import generate_test_cases
 from app.rag.loaders.load_web_docs import load_docs_from_web
-from app.generator import generate_answer
 from app.evaluation import rank_prompts_with_elo
 from app.prompts_generation import PromptGenerator
 
 def generate_ranked_prompts(task_description):
-  print(task_description)
+  # print(task_description)
   # task_description = "Write me prompts to find information about few shot learning"
   # Here we generate test_cases and prompt_candidates
 
