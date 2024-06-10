8c8d2922b7bb967cb4f5e589536debb1339af44e
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-10 08:45:53 +0300

    run battles with montecarlo

--- app/evaluation/prompt_ranking_elo.py
+++ app/evaluation/prompt_ranking_elo.py
@@ -1,7 +1,7 @@
 from app.evaluation import EloRanker
 
-def rank_prompts_with_elo(task_description, prompt_candidates, test_cases, retriever):
+def rank_prompts_with_elo(task_description, prompt_candidates, test_cases, retriever, num_battles=1000, sample_amount=1000):
   elo_ranker = EloRanker(task_description, prompt_candidates, test_cases, retriever)
-  elo_ranker.calculate_elo_ranks()
+  elo_ranker.run_simulation(num_battles, sample_amount)
   ranked_prompts = elo_ranker.rank_prompts()
   return ranked_prompts
