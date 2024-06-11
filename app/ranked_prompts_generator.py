50d6f6e91bc5a1f5892acfe5bc3b6d88451b8ffd
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-11 07:23:54 +0300

    get answer from context

--- app/ranked_prompts_generator.py
+++ app/ranked_prompts_generator.py
@@ -3,6 +3,19 @@ from app.rag.loaders.load_web_docs import load_docs_from_web
 from app.evaluation import rank_prompts_with_elo
 from app.prompts_generation import PromptGenerator
 
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain.prompts.chat import ChatPromptTemplate
+from app.utils.chat_models import ModelFactory
+from app.utils.docs_utils import format_docs_to_text
+
+ANSWER_GENERATION_TEMPERATURE = 0.7
+model_factory = ModelFactory(
+                    temperature=ANSWER_GENERATION_TEMPERATURE, 
+                    openai_model_name="gpt-3.5-turbo"
+                  )
+
+
 def generate_ranked_prompts(task_description):
   # print(task_description)
   # task_description = "Write me prompts to find information about few shot learning"
@@ -18,3 +31,32 @@ def generate_ranked_prompts(task_description):
   ranked_prompts = rank_prompts_with_elo(task_description, prompt_candidates, test_cases, retriever)
   
   return ranked_prompts
+
+def generate_answer(prompt: str):
+    retriever = load_docs_from_web()
+
+    prompt_template = ChatPromptTemplate.from_template(
+    """
+        Use the following context to answer the question.
+        If you don't know the answer, just say that you don't know, don't try to make up an answer.
+        Context: {context}
+        Question: {prompt_candidate}
+        Answer:"""
+    )
+
+    context_docs = retriever.invoke(prompt)
+    context_text = format_docs_to_text(context_docs)
+
+    llm = model_factory.get_chat_openai()
+
+    prompt_input_variables = {
+        "context": context_text, 
+        "prompt_candidate": prompt,
+    }
+
+    chain = prompt_template | llm | StrOutputParser()
+
+    # Generate answer from RAG chain
+    answer = chain.invoke(prompt_input_variables)
+  
+    return answer
