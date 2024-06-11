77752d58f8811912d61822ee2b613cadca32b629
Hillary Kipkemoi <tenskap@gmail.com>
2024-06-11 07:23:33 +0300

    add generate answer functionality in streamlit

--- run.py
+++ run.py
@@ -6,50 +6,63 @@ import time
 import os
 os.chdir("./")
 
-from app import generate_ranked_prompts
+from app.ranked_prompts_generator import generate_ranked_prompts, generate_answer
 
-
+st. set_page_config(layout="wide") 
 
 st.title('Automatic Prompts Generator')
 
-# Hardcoded ranked prompts
+# Create two columns
+col1, col_space, col2 = st.columns([2, 0.5, 2])
 
-# ranked_prompts = [
-#   ("Describe the principles and applications of few-shot learning.", 1060, 100),
-#   ("Describe the unique approach of a learning method that involves acquiring knowledge from only a limited amount of data.", 1029, 97),
-#   ("Please provide detailed explanations, definitions, and examples to help clarify the concept of few-shot learning.", 999, 94),
-#   ("Describe the principles and techniques involved in a unique approach to machine learning that aims to minimize the amount of training data required.", 969, 91),
-#   ("Describe the unique approach in machine learning that involves learning from a limited number of examples.", 940, 88)
-# ]
+# In the first column, display the prompt generation
+with col1:
+  # Add a text box for user input
+  user_prompt = st.text_input("Enter your task or question to get ranked prompts")
 
-# Add a text box for user input
-user_prompt = st.text_input("Enter your task description or question to get alternative ranked prompts")
+  if st.button('Generate Prompts'):
+    with st.spinner('Generating prompts...'):
+      ranked_prompts = generate_ranked_prompts(user_prompt)
+      st.session_state['ranked_prompts'] = ranked_prompts  # Store the prompts in the session state
 
-if st.button('Generate Prompts'):
-  with st.spinner('Generating prompts...'):
-    ranked_prompts = generate_ranked_prompts(user_prompt)
-
-    # Add a delay
-    # time.sleep(2)
-
+  # Check if the prompts exist in the session state
+  if 'ranked_prompts' in st.session_state:
     # Display the ranked prompts along with the percentage score
-    for i, (prompt, rank, percentage) in enumerate(ranked_prompts, start=1):
+    for i, (prompt, rank, percentage) in enumerate(st.session_state['ranked_prompts'], start=1):
       markdown_header =  (
-          f"<div style='background-color: rgb(37, 39, 42); padding: 10px;'>"
-              f"Prompt #{i} &nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;🎯&nbsp;"
-                    f"{percentage}% &nbsp;&nbsp;&nbsp;&nbsp;"
-                    f" | &nbsp;&nbsp;&nbsp;&nbsp; ELO: {rank}"
-          f"</div>"
+        f"<div style='background-color: rgb(37, 39, 42); padding: 10px;'>"
+          f"Prompt #{i} &nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;🎯&nbsp;"
+            f"{percentage}% &nbsp;&nbsp;&nbsp;&nbsp;"
+            f" | &nbsp;&nbsp;&nbsp;&nbsp; ELO: {rank}"
+        f"</div>"
       )
       st.markdown(markdown_header, unsafe_allow_html=True)
-      # st.markdown(f"<div style='background-color: rgb(70, 72, 74); padding: 10px;'>{prompt}</div>", unsafe_allow_html=True)
       with stylable_container(
         "codeblock",
         """
         code {
-            white-space: pre-wrap !important;
-            font-family: arial;
+          white-space: pre-wrap !important;
+          font-family: arial;
         }
         """,
       ):
-          st.code(prompt, language=None)
+        st.code(prompt, language=None)
+
+# In the second column, add a text box for the prompt and display the generated answer
+with col2:
+  # Add a text box for the prompt
+  prompt = st.text_input("Enter a prompt to get an answer. (The default context is prompt engineering)")
+
+  if st.button('Generate Answer'):
+    with st.spinner('Generating answer...'):
+      answer = generate_answer(prompt)  # Generate the answer for the prompt
+      with stylable_container(
+        "codeblock",
+        """
+        code {
+          white-space: pre-wrap !important;
+          font-family: arial;
+        }
+        """,
+      ):
+        st.code(answer, language=None)
